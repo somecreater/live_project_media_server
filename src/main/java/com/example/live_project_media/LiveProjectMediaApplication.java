9effabf8503b594d7bd35327de3f0ca221db50ce
somecreater <127456520+somecreater@users.noreply.github.com>
2026-04-10 15:22:16 +0900

    Initial commit

---
+++ src/main/java/com/example/live_project_media/LiveProjectMediaApplication.java
@@ -0,0 +1,13 @@
+package com.example.live_project_media;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveProjectMediaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiveProjectMediaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/live_project_media/LiveProjectMediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.live_project_media;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveProjectMediaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
