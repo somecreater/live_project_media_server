129a51d0b3952ca5328455bfbc4eb614859ec950
somecreater <127456520+somecreater@users.noreply.github.com>
2026-04-28 09:34:43 +0900

    미디어 처리에 사용되는 명령어 실행을 위한 Service 메소드 구현

--- src/main/java/com/example/live_project_media/encoding/EncodingService.java
+++ src/main/java/com/example/live_project_media/encoding/EncodingService.java
@@ -4,14 +4,37 @@ import com.example.live_project_media.dto.VideoValidationEvent;
 import com.example.live_project_media.encoding.Interface.EncodingServiceInterface;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Service;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
 @Service
 public class EncodingService implements EncodingServiceInterface {
+
+  private final S3Client s3Client;
+  private final S3Presigner s3Presigner;
+
+  @Value("${app.r2.video-bucket-name}")
+  private String bucket_name;
+  @Value("${app.r2.original-video-bucket-name}")
+  private String original_video_folder;
+  @Value("${app.r2.transcoding-video-bucket-name}")
+  private String transcoding_video_folder;
+  @Value("${app.file.video_limit_size}")
+  private Long video_limit_size;
+
+  /**aws ebs 미 구성에 따른 임시 파일 경로*/
+  private final String TMP_FILE_DIRECTORY="";
 
   @KafkaListener(
           topics = "video-complete-topic",
@@ -28,5 +51,24 @@ public class EncodingService implements EncodingServiceInterface {
       } catch (Exception e) {
           log.error("Video consume failed", e);
       }
+  }
+
+
+  public int runProcess(List<String> command) throws IOException, InterruptedException{
+      ProcessBuilder processBuilder = new ProcessBuilder(command);
+      processBuilder.redirectErrorStream(true);
+
+      Process process = processBuilder.start();
+
+      try (BufferedReader reader = new BufferedReader(
+              new InputStreamReader(process.getInputStream())
+      )) {
+          String line;
+          while ((line = reader.readLine()) != null) {
+              log.info("[ffmpeg] {}", line);
+          }
+      }
+
+      return process.waitFor();
   }
 }
--- src/main/java/com/example/live_project_media/encoding/Interface/EncodingServiceInterface.java
+++ src/main/java/com/example/live_project_media/encoding/Interface/EncodingServiceInterface.java
@@ -3,8 +3,13 @@ package com.example.live_project_media.encoding.Interface;
 import com.example.live_project_media.dto.VideoValidationEvent;
 import org.springframework.kafka.support.Acknowledgment;
 
+import java.io.IOException;
+import java.util.List;
+
 /**동영상 인코딩 기능(2026-04-27)*/
 public interface EncodingServiceInterface {
   /**Kafka로부터 영상 업로드 및 검증 완료 메시지 수신*/
   public void consumerValidateMessage(VideoValidationEvent encodeEvent, Acknowledgment ack);
+  /**미디어 처리를 위한 명령어 실행*/
+  public int runProcess(List<String> command) throws IOException, InterruptedException;
 }
